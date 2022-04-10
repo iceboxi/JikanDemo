f39c0adf1d4b29d93e9eab8d4a55df2c2258117d
iceboxi <iceboxidev@gmail.com>
2022-04-10 14:06:31 +0800

    feat: init libs

---
+++ JikanDemo/Manager/LibsManager.swift
@@ -0,0 +1,43 @@
+//
+//  LibManager.swift
+//  JikanDemo
+//
+//  Created by ice on 2022/4/10.
+//
+
+import Foundation
+import CocoaLumberjackSwift
+import IQKeyboardManagerSwift
+import FirebaseCrashlytics
+import Firebase
+
+class LibsManager: NSObject {
+
+    /// The default singleton instance.
+    static let shared = LibsManager()
+
+    private override init() {
+        super.init()
+
+    }
+
+    func setupLibs() {
+        let libsManager = LibsManager.shared
+        libsManager.setupCocoaLumberjack()
+        libsManager.setupAnalytics()
+        libsManager.setupKeyboardManager()
+    }
+
+    func setupKeyboardManager() {
+        IQKeyboardManager.shared.enable = true
+    }
+    
+    func setupCocoaLumberjack() {
+        logInitail()
+    }
+
+    func setupAnalytics() {
+        FirebaseApp.configure()
+        FirebaseConfiguration.shared.setLoggerLevel(.min)
+    }
+}
---
+++ JikanDemo/Manager/LogManager.swift
@@ -0,0 +1,87 @@
+//
+//  LogManager.swift
+//  JikanDemo
+//
+//  Created by ice on 2022/4/10.
+//
+
+import Foundation
+import CocoaLumberjackSwift
+
+public func logInitail() {
+    let logger = DDOSLogger.sharedInstance
+    logger.logFormatter = CustomFormat(false)
+    DDLog.add(logger)
+    
+    let fileLogger: DDFileLogger = DDFileLogger()
+    fileLogger.logFormatter = CustomFormat(true)
+    fileLogger.rollingFrequency = TimeInterval(60*60*24)
+    fileLogger.logFileManager.maximumNumberOfLogFiles = 7
+    DDLog.add(fileLogger)
+}
+
+public func logDebug(_ message: @autoclosure () -> String,
+                     file: StaticString = #file,
+                     function: StaticString = #function,
+                     line: UInt = #line,
+                     tag: Any? = nil) {
+    DDLogDebug(message(), file: file, function: function, line: line, tag: tag)
+}
+
+public func logError(_ message: @autoclosure () -> String,
+                     file: StaticString = #file,
+                     function: StaticString = #function,
+                     line: UInt = #line,
+                     tag: Any? = nil) {
+    DDLogError(message(), file: file, function: function, line: line, tag: tag)
+}
+
+public func logInfo(_ message: @autoclosure () -> String,
+                    file: StaticString = #file,
+                    function: StaticString = #function,
+                    line: UInt = #line,
+                    tag: Any? = nil) {
+    DDLogInfo(message(), file: file, function: function, line: line, tag: tag)
+}
+
+public func logVerbose(_ message: @autoclosure () -> String,
+                       file: StaticString = #file,
+                       function: StaticString = #function,
+                       line: UInt = #line,
+                       tag: Any? = nil) {
+    DDLogVerbose(message(), file: file, function: function, line: line, tag: tag)
+}
+
+public func logWarn(_ message: @autoclosure () -> String,
+                    file: StaticString = #file,
+                    function: StaticString = #function,
+                    line: UInt = #line,
+                    tag: Any? = nil) {
+    DDLogWarn(message(), file: file, function: function, line: line, tag: tag)
+}
+
+class CustomFormat: NSObject, DDLogFormatter {
+    private var showDetail: Bool
+    init(_ showDetail: Bool) {
+        self.showDetail = showDetail
+    }
+    
+    func format(message logMessage: DDLogMessage) -> String? {
+        var level = ""
+        switch logMessage.flag {
+        case .debug: level = "⚫️"
+        case .info: level = "⚪️"
+        case .warning: level = "🟡"
+        case .error: level = "🔴"
+        case .verbose: level = "🟤"
+        default: level = "🔵"
+        }
+        
+        var result = "\(level)"
+        if showDetail {
+            result += " \(logMessage.fileName):\(logMessage.function ?? ""):\(logMessage.line) [\(String(describing: logMessage.representedObject))]"
+        }
+        return "\(result) \(logMessage.message)"
+    }
+}
+
